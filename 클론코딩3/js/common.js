160d61b9994775a12ed193b97c33776a2a90d71a
qja7767 <qlcjs7789@naver.com>
2022-06-05 18:14:29 +0900

    test

--- 클론코딩3/js/common.js
+++ 클론코딩3/js/common.js
@@ -10,6 +10,7 @@ $(function() {
     }
   });
 
+  // GNB 마우스엔터 위치 인식후 높이값 조정 및 클래스 부여
   function gnbHover(elem1, elem2, elem3){
     $(elem1).on({
       mouseenter: function(){
@@ -27,7 +28,7 @@ gnbHover("li.n1", "a.n1", "472px");
 gnbHover("li.n2", "a.n2", "700px");
 gnbHover("li.n3", "a.n3", "335px");
 gnbHover("li.n4", "a.n4", "484px");
-gnbHover("li.n5", "a.n5", "633px");
+gnbHover("li.n5", "a.n5", "645px");
 gnbHover("li.n6", "a.n6", "335px");
 
 });
